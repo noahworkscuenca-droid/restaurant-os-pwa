db707d2d7ff851b7444b921e793df1b8cc5ead80
noahworkscuenca-droid <noahworkscuenca@gmail.com>
2026-07-01 21:25:41 -0600

    Add sw.js

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE = 'ros-v1';
+const ASSETS = ['./', './index.html', './config.js', './manifest.json', './icon-192.png', './icon-512.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then(cached =>
+      cached || fetch(e.request).then(res => {
+        // Cachea CDN (tesseract, supabase, fonts) para uso offline posterior
+        if (res.ok && (e.request.url.startsWith(self.location.origin) || /cdn|unpkg|jsdelivr|gstatic|googleapis/.test(e.request.url))) {
+          const clone = res.clone();
+          caches.open(CACHE).then(c => c.put(e.request, clone));
+        }
+        return res;
+      }).catch(() => caches.match('./index.html'))
+    )
+  );
+});
