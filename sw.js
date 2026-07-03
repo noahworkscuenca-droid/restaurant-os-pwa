d43f2689102159b37a5d034ecf2f6062a0e08a80
noahworkscuenca-droid <noahworkscuenca@gmail.com>
2026-07-03 10:37:58 -0600

    v4: estado de pago, edicion de facturas, filtros en historial

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'ros-v3';
+const CACHE = 'ros-v4';
 const ASSETS = ['./', './index.html', './config.js', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
