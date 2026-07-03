ab128c0f035e1158a66dde059ae258da18a86dc5
noahworkscuenca-droid <noahworkscuenca@gmail.com>
2026-07-03 00:02:57 -0600

    v2: acceso por codigo, subida de archivos, dashboard de gastos

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'ros-v2';
+const CACHE = 'ros-v3';
 const ASSETS = ['./', './index.html', './config.js', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
