8a7a0123f886b97fcfba7093e0efdfed23f737e4
noahworkscuenca-droid <noahworkscuenca@gmail.com>
2026-07-16 13:19:40 -0600

    v8: reactivar Pages, aprendizaje de proveedores, exportar CSV

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'ros-v7';
+const CACHE = 'ros-v8';
 const ASSETS = ['./', './index.html', './config.js', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
