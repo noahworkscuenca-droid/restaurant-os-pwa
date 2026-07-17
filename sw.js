5512709a888c66c8f2db58cce713994cede6fdb6
noahworkscuenca-droid <noahworkscuenca@gmail.com>
2026-07-17 12:54:21 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'ros-v9';
+const CACHE = 'ros-v10';
 const ASSETS = ['./', './index.html', './config.js', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
