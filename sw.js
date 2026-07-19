5ad98ef8d3981bc9313381d89e7667f32861d3d1
noahworkscuenca-droid <noahworkscuenca@gmail.com>
2026-07-19 12:54:59 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'ros-v12';
+const CACHE = 'ticksy-v13';
 const ASSETS = ['./', './index.html', './config.js', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
