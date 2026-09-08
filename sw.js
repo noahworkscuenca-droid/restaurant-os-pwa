94c7ec3ec3556f2d4ce3be90fb224a83bbc0fabf
noahworkscuenca-droid <noahworkscuenca@gmail.com>
2026-09-07 18:33:23 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'ticksy-v20';
+const CACHE = 'ticksy-v21';
 const ASSETS = ['./', './index.html', './config.js', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
